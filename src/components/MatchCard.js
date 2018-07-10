a95a29343fb9e38c782b1b680e307bbfe8172034
Joshua Jackson <joshua.j@mmtdigital.co.uk>
2018-07-10 12:32:31 +0100

    Added flags

--- src/components/MatchCard.js
+++ src/components/MatchCard.js
@@ -29,20 +29,12 @@ const MatchCard = (props) =>
 {
   const matchData = props.matchData;
 
-
-  /* 
-    * header element should be 3 column grid. left and right columns containing
-    * flag and country name. Middle column the score.
-  
-
-    * Extract country flag grabbing to a seperate function that renders out an image with the require link
-  */
   return (
     <section className={styles.card}>
 
       <header className={styles.header}>
         <span className={styles.teamHeading}>
-          <img src="http://www.countryflags.io/ru/flat/64.png" alt="Russian Flag" />
+          <img src={require(`../resources/images/flags/${matchData.firstTeam.replace(/\s+/g, '-')}.png`)} alt={`${matchData.firstTeam} flag`} />
           <h1>{matchData.firstTeam}</h1>
         </span>
 
@@ -52,7 +44,7 @@ const MatchCard = (props) =>
 
         <span className={styles.teamHeading}>
           <h1>{matchData.secondTeam}</h1>
-          <img src="http://www.countryflags.io/sa/flat/64.png" alt="Saudi Arabian Flag" />
+          <img src={require(`../resources/images/flags/${matchData.secondTeam.replace(/\s+/g, '-')}.png`)} alt={`${matchData.secondTeam} flag`} />
         </span>
       </header>
 
